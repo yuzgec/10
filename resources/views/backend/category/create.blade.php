b81189aafc9f08116fdd79d38c99c73d8d50179d
yuzgec <olcayy@gmail.com>
2024-12-16 21:12:23 +0300

    Fix

--- resources/views/backend/category/create.blade.php
+++ resources/views/backend/category/create.blade.php
@@ -151,7 +151,10 @@
     @foreach($language as $lang)
         <script type="text/javascript">
             CKEDITOR.replace( 'desc:{{ $lang->lang }}', {
-                filebrowserUploadUrl: "{{ route('page.index', ['_token' => csrf_token()]) }}",
+                filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+                filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token={{ csrf_token() }}',
+                filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+                filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token={{ csrf_token() }}',
                 filebrowserUploadMethod: 'form',
                 allowedContent: true,
                 height : 400,
