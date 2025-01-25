a73ddd67efab39bb70a48e7ebf9de615999c4ffa
yuzgec <olcayy@gmail.com>
2025-01-25 07:23:56 +0300

    Fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -315,7 +315,7 @@
                                                         </h2>
                                                     </div>
                                                     <div class="!relative">
-                                                        {{ $item->short}}
+                                                        {!! $item->short!!}
                                                     </div>
                                                 </div>
 
