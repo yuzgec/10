d911c53d4d59033f6d583472ce535c80082b3a58
yuzgec <olcayy@gmail.com>
2025-01-28 01:04:09 +0300

    Fix

--- app/Providers/ViewShareProvider.php
+++ app/Providers/ViewShareProvider.php
@@ -22,39 +22,39 @@ class ViewShareProvider extends ServiceProvider
     public function boot(): void
     {
      
-        $settings = Cache::remember('settings',now()->addYear(5), function () {
+        $settings = Cache::remember('settings',now()->addDay(1), function () {
             return config()->set('settings', Setting::pluck('value','item')->all());
         });
 
-        $status = Cache::remember('status',now()->addYear(5), function () {
+        $status = Cache::remember('status',now()->addDay(1), function () {
             return collect(StatusEnum::cases());
         });
 
-        $services = Cache::remember('services',now()->addYear(5), function () {
+        $services = Cache::remember('services',now()->addDay(1), function () {
             return Service::with(['getCategory','media'])->active()->lang()->rank()->get();
         });
 
-        $pages = Cache::remember('pages',now()->addYear(5), function () {
+        $pages = Cache::remember('pages',now()->addDay(1), function () {
             return Page::with(['getCategory','media'])->active()->lang()->rank()->get();
         });
 
-        $blog = Cache::remember('blogs',now()->addYear(5), function () {
+        $blog = Cache::remember('blogs',now()->addDay(1), function () {
             return Blog::with(['getCategory','media'])->active()->lang()->rank()->get();
         });
 
-        $language = Cache::remember('language',now()->addYear(5), function () {
+        $language = Cache::remember('language',now()->addDay(1), function () {
             return Language::active()
                 ->select(['id', 'lang', 'native', 'rank'])
                 ->orderBy('rank')
                 ->get();
         });
 
-        $teams = Cache::remember('teams',now()->addYear(5), function () {
+        $teams = Cache::remember('teams',now()->addDay(1), function () {
             return Page::with(['getCategory','media'])->active()->lang()->rank()->get();
         });
 
 
-        $categories = Cache::remember('categories',now()->addYear(5), function () {
+        $categories = Cache::remember('categories',now()->addDay(1), function () {
             return Category::withCount(['pages', 'services', 'blogs', 'faqs','media','teams', 'products'])->lang()->get()->toFlatTree();
         });
 
--- resources/views/components/dashboard/index/image.blade.php
+++ resources/views/components/dashboard/index/image.blade.php
@@ -2,7 +2,7 @@
 <td>
     <div class="avatar-list avatar-list-stacked">
         <a data-fslightbox="gallery" href="{{ $model->getFirstMediaUrl('page', 'thumb')}}">
-            <img src="{{ $model->getFirstMediaUrl('page', 'icon')}}" class="avatar me-2">
+            <img src="{{ $model->getFirstMediaUrl('page', 'icon')}}">
         </a>
         @if($count > 0)
             <span class="avatar avatar-sm rounded">+{{ $count }}</span>
